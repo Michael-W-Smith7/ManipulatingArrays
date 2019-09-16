5f4dc5cdb293ff1cf6d79dbb209072c156cbe5a7
Michael <Michael.W.Smith77@outlook.com>
2019-09-16 10:39:18 -0400

    Added all files

---
+++ Operation.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manipulating_Arrays
+{
+    class Operation
+    {
+        public static int Count(int[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                count++;
+            }
+            return count;
+        }
+
+        public static int Sum(int[] array)
+        {
+            int sum = 0;
+            
+            for(int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+        public static double Mean(int sum, int count)
+        {
+            double mean = sum / count;
+            
+
+            return mean;
+        }
+        public static void Reverse(int[] array)
+        {
+            for (int i = array.Length -1; i>=0; i--)
+            {
+                Console.Write("{0}, ",array[i]);
+                
+            }
+        }
+
+        internal static void Sort(int[] array)
+        {
+            
+                int i, j, temp;
+                int N = array.Length;
+
+            for (j = N - 1; j > 0; j--)
+            {
+                for (i = 0; i < j; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                    }
+                }
+            }
+            for (i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0}, ", array[i]);
+                
+            }
+            Console.WriteLine();
+        }
+
+        internal static void Rotate(int[] array, int dir, int move)
+        {
+
+            
+            int temp, i;
+            int[] arraycopy = new int[array.Length];
+
+            
+            
+
+                for (i = 0; i < array.Length; i++)
+            {
+                if (dir == 1)
+                {
+                    //left
+                    if (move > array.Length)
+                    {
+                        move %= array.Length;
+                    }
+
+                    if (i + move < array.Length)
+                    {
+                        temp = array[i];
+                        arraycopy[i] = array[i + move];
+                        array[i + move] = temp;
+                    }
+                    if (i + move >= array.Length)
+                    {
+                        temp = array[i];
+                        arraycopy[i] = array[(i - array.Length) + move];
+                        array[(i - array.Length) + move] = temp;
+                    }
+                }
+                if (dir == 2)
+                {
+
+                    //right
+
+                    if (move > array.Length)
+                    {
+                        move %= array.Length;
+                    }
+                    if (i - move >= 0)
+                    {
+                        temp = array[i];
+                        arraycopy[i] = array[i - move];
+                        array[i - move] = temp;
+                    }
+                    if (i - move < 0)
+                    {
+                        temp = array[i];
+                        arraycopy[i] = array[(i + array.Length) - move];
+                        array[(i + array.Length) - move] = temp;
+                    }
+                }
+
+            }
+            for (i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0}, ", arraycopy[i]);
+
+            }
+        }
+
+        internal static int[] Arraychoose(int[] arra, int[] arrb, int[] arrc)
+        {
+            int i = 0;
+            while (i!=1)
+            {
+                Console.WriteLine("Please choose which array you would like to use.");
+                Console.WriteLine("Type A for Array A");
+                Console.WriteLine("Type B for Array B");
+                Console.WriteLine("Type C for Array C");
+                Console.Write("I would like Array: ");
+                string checkme = Console.ReadLine();
+                
+                if (checkme == "A" || checkme == "a")
+                {
+                    return arra;
+                    
+                }
+                else if (checkme == "B" || checkme == "B")
+                {
+                    return arrb;
+                    
+                }
+                else if (checkme == "C" || checkme == "c")
+                {
+                    return arra;
+                    
+                }
+                else if (checkme != "A")
+                {
+                    Console.WriteLine("\nThats not quite right try again.\n");
+                }
+
+            }
+            Console.WriteLine("Somethings not right lets just use Array C Who doesnt like pi anyway.");
+            return arrc;
+        }
+
+        public static (int, int) Directions()
+        {
+            int dir=0, move, i=0;
+           
+
+            while (i != 1)
+            {
+                Console.WriteLine("Would you like to move the array to the left or to the right?");
+                Console.Write("Move it to the: ");
+                string checkme = Console.ReadLine();
+                if (checkme == "right" || checkme == "Right" || checkme == "r" || checkme == "R")
+                {
+                    dir = 2;
+                    i++;
+                }
+                else if (checkme == "left" || checkme == "Left" || checkme == "l" || checkme == "L")
+                {
+                    dir = 1;
+                    i++;
+                }
+                else if (checkme != "left" || checkme != "Left" || checkme != "l" || checkme != "L"
+                    || checkme != "right" || checkme != "Right" || checkme != "r" || checkme != "R")
+                {
+                    Console.WriteLine("\nI didnt understand that. please check your spelling\n");
+                }
+            }
+                Console.WriteLine("\n How many spaces would you like to move it?");
+                Console.Write("Move it over: ");
+
+                while (!int.TryParse(Console.ReadLine(), out move))
+                {
+                    Console.WriteLine("\nI dont think that was a number. try again");
+                    Console.Write("Move it over: ");
+
+                }
+                return (dir, move);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manipulating_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oper = new Operation();
+            int[] arra = new int[6]
+            {
+                0,2,4,6,8,10
+            };
+            int[] arrb = new int[5]
+            {
+                1,3,5,7,9
+            };
+            int[] arrc = new int[12]
+            {
+                3,1,4,1,5,9,2,6,5,3,5,9
+            };
+            
+            Console.WriteLine("Counting summing and computing the mean");
+            Console.WriteLine("---------------------------------------");
+
+            Console.WriteLine("Array A count, Sum, and Mean\n");
+
+            var count = Operation.Count(arra);
+            var sum = Operation.Sum(arra);
+            var mean = Operation.Mean(sum, count);
+           
+            Console.WriteLine($"Array A has {count} integers inside.");
+            Console.WriteLine($"The sum of the array is {sum}");
+            Console.WriteLine($"The mean of the array is {mean}");
+
+            count = Operation.Count(arrb);
+            sum = Operation.Sum(arrb);
+            mean = Operation.Mean(sum, count);
+
+            Console.WriteLine($"\nArray B has {count} integers inside.");
+            Console.WriteLine($"The sum of the array is {sum}");
+            Console.WriteLine($"The mean of the array is {mean}");
+
+            count = Operation.Count(arrc);
+            sum = Operation.Sum(arrc);
+            mean = Operation.Mean(sum, count);
+
+            Console.WriteLine($"\nArray C has {count} integers inside.");
+            Console.WriteLine($"The sum of the array is {sum}");
+            Console.WriteLine($"The mean of the array is {mean}");
+
+            Console.WriteLine("\nReversing arrays");
+            Console.WriteLine("---------------------------------------\n");
+
+            Console.WriteLine("Array A in reverse is");
+            Operation.Reverse(arra);
+
+            Console.WriteLine("\n\nArray B in reverse is");
+            Operation.Reverse(arrb);
+
+            Console.WriteLine("\n\nArray C in reverse is");
+            Operation.Reverse(arrc);
+            Console.WriteLine("");
+
+
+
+            Console.WriteLine("\nRotating arrays");
+            Console.WriteLine("---------------------------------------\n");
+            int[] arrvar = Operation.Arraychoose(arra, arrb, arrc);
+            (int dir, int move) = Operation.Directions();
+            move = Math.Abs(move);
+            Operation.Rotate(arrvar, dir, move);
+
+            Console.WriteLine("\nSorting pie arrays");
+            Console.WriteLine("---------------------------------------\n");
+            Operation.Sort(arrc);
+
+
+            
+        }
+    }
+}
